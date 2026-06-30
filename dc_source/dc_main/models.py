ebe8a79c70a749408a7bd562bad53b6dc8deb4b0
seb898828 <sebrollsroyce@proton.me>
2026-06-30 13:20:57 +0100

    expanded models and made them better

--- dc_source/dc_main/models.py
+++ dc_source/dc_main/models.py
@@ -12,26 +12,26 @@ class Profile(models.Model):
         ("admin", "Admin")
     ]
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     role = models.CharField(max_length=20, choices=ROLE_CHOICES)
 
-    name = models.CharField(max_length=16)
-    # Photo = models.ImageField(upload_to='photos/')
+    name = models.CharField(max_length=32)
+    photo = models.ImageField(upload_to='photos/', blank=True, null=True)
     grade = models.CharField(max_length=16)
-    position = models.CharField(max_length=16)
-    location = models.CharField(max_length=16)
-    history = models.TextField(max_length=256)
-    capabilities_and_attributes = models.TextField(max_length=256)
-    career_aspirations = models.TextField(max_length=256)
-    development_needs = models.TextField(max_length=256)
-    mobility = models.TextField(max_length=128)
-    succession_plan = models.TextField(max_length=256)
-    successor = models.CharField(max_length=32)
+    position = models.CharField(max_length=32)
+    location = models.CharField(max_length=32)
+    history = models.TextField(blank=True)
+    capabilities_and_attributes = models.TextField(blank=True)
+    career_aspirations = models.TextField(blank=True)
+    development_needs = models.TextField(blank=True)
+    mobility = models.CharField(max_length=128)
+    succession_plan = models.TextField(blank=True)
+    successor = models.ForeignKey(User, null = True, on_delete=models.SET_NULL, related_name="successor_profile")
     potential_grade = models.CharField(max_length=16)
     dev_cell_rating = models.CharField(max_length=16)
     performance_rating = models.CharField(max_length=16)
-    wants = models.TextField(max_length=256)
-    needs = models.TextField(max_length=256)
+    wants = models.TextField(blank=True)
+    needs = models.TextField(blank=True)
 
 
 class Relationship(models.Model):
@@ -47,13 +47,14 @@ class Relationship(models.Model):
 
 
 class Action(models.Model):
-    action = models.CharField(max_length=16)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="actions")
+    action = models.CharField(max_length=32)
     action_owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    next_move = models.TextField(max_length=128)
+    next_move = models.TextField(blank=True)
     action_status = models.CharField(max_length=32)
 
 
 class Review(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     review_date = models.DateField()
-    review_information = models.TextField(max_length=32)
+    review_information = models.TextField(blank=True)
