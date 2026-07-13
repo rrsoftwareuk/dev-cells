8f58b2f4f8620a2a62a5854f0219370d81a24c4e
Daniel <archedheart2@gmail.com>
2026-07-13 14:02:02 +0100

    Changed which information is displayed on the admin pages for actions, reviews and for profiles and relationships

--- dc_source/dc_main/admin.py
+++ dc_source/dc_main/admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
+from django.contrib.admin import actions
+
 from .models import Profile, Relationship, Action, Review
 
 # Register your models here.
 
-admin.site.register(Profile)
-admin.site.register(Relationship)
-admin.site.register(Action)
-admin.site.register(Review)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('name', 'position', 'role', 'dev_cell_rating', 'performance_rating', 'grade')
+
+class RelationshipAdmin(admin.ModelAdmin):
+    list_display = ('from_user', 'to_user')
+
+class ActionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'action_owner', 'action_status')
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('user', 'review_date')
+
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Relationship, RelationshipAdmin)
+admin.site.register(Action, ActionAdmin)
+admin.site.register(Review, ReviewAdmin)
